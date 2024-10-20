0239518c175073b9c05c30c46dcfa28790b40e1b
Dajohnson000 <daj22c@fsu.edu>
2024-10-20 15:24:55 -0400

    Format code to not hurt my brain

--- dice.js
+++ dice.js
@@ -16,61 +16,60 @@ These methods are available:
 */
 
 class Die {
-    
-    constructor(size) {
-        this.size = size;
-        this.sides = 6;
-        this.frozen = false;
-        this.place(random(width),random(height));
-        this.roll();
-    }
+  constructor(size) {
+    this.size = size;
+    this.sides = 6;
+    this.frozen = false;
+    this.place(random(width), random(height));
+    this.roll();
+  }
 
-    place(x,y) {
-        this.x = x;
-        this.y = y;
-    }
+  place(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    display() {
-        //rectMode(CENTER);
-        textAlign(CENTER, CENTER);
-        fill("white");
-        strokeWeight(3);
-        rect(this.x,this.y,this.size,this.size,8);
-        if (this.frozen) {
-            fill("silver");
-        } else {
-            fill("black");
-        }
-        textSize(30);
-        text(this.value, this.x, this.y, this.size, this.size);
+  display() {
+    rectMode(CENTER);
+    textAlign(CENTER, CENTER);
+    fill("white");
+    strokeWeight(3);
+    rect(this.x, this.y, this.size, this.size, 8);
+    if (this.frozen) {
+      fill("silver");
+    } else {
+      fill("black");
     }
+    textSize(30);
+    text(this.value, this.x, this.y, this.size, this.size);
+  }
 
-    roll() {
-        if (! this.frozen) 
-            this.value = floor(random(this.sides)+1);
-    }
+  roll() {
+    if (!this.frozen) this.value = floor(random(this.sides) + 1);
+  }
 
-    freeze() {
-        this.frozen = true;
-    }
+  freeze() {
+    this.frozen = true;
+  }
 
-    unfreeze() {
-        this.frozen = false;
-    }
+  unfreeze() {
+    this.frozen = false;
+  }
 
-    toggleFreeze() {
-        this.frozen = ! this.frozen;
-    }
+  toggleFreeze() {
+    this.frozen = !this.frozen;
+  }
 
-    isTouched(x,y) {
-        let halfsize = this.size/2;
-        if (x >= (this.x-halfsize) && 
-            x <= (this.x+halfsize) && 
-            y >= (this.y-halfsize) && 
-            y <= (this.y+halfsize)
-        ) {
-            return true;
-        } 
-        return false;
+  isTouched(x, y) {
+    let halfsize = this.size / 2;
+    if (
+      x >= this.x - halfsize &&
+      x <= this.x + halfsize &&
+      y >= this.y - halfsize &&
+      y <= this.y + halfsize
+    ) {
+      return true;
     }
+    return false;
+  }
 }
--- sketch.js
+++ sketch.js
@@ -9,24 +9,27 @@ function setup() {
   for (let i = 0; i < numberOfDice; i++) {
     dice[i] = new Die(50); // argument is the size of the die
   }
-  console.log('i am here');
-
+  console.log("i am here");
 }
 
 function draw() {
   background("Pink");
-  text('Pig', windowWidth/2, 100)
-  text('Game description: Try to get to 100 points from rolling the dice in the least amount of rolls.', windowWidth/2, 200);
+  text("Pig", windowWidth / 2, 100);
+  text(
+    "Game description: Try to get to 100 points from rolling the dice in the least amount of rolls.",
+    windowWidth / 2,
+    200
+  );
   // loop over the array and place+display each die
   for (let i = 0; i < dice.length; i++) {
     const die = dice[i]; // 'die' is a temporary variable for the current array item
-    die.place(die.size*1.2*i+die.size, die.size*2); // place the die neatly in the row
+    die.place(die.size * 1.2 * i + die.size, die.size * 2); // place the die neatly in the row
     die.display(); // actually draw it on screen
   }
-  text('Total', 200, 300);
-  text(sum, 200, 350); 
-  text('Rolls', 300, 300);
-  text( rolls, 300, 350);
+  text("Total", 200, 300);
+  text(sum, 200, 350);
+  text("Rolls", 300, 300);
+  text(rolls, 300, 350);
 }
 
 function mouseClicked() {
@@ -34,7 +37,7 @@ function mouseClicked() {
   for (let i = 0; i < dice.length; i++) {
     const die = dice[i];
     // if the cursor is over the current die, freeze/unfreeze that die
-    if (die.isTouched(mouseX,mouseY)) {
+    if (die.isTouched(mouseX, mouseY)) {
       die.toggleFreeze();
     }
   }
@@ -54,19 +57,16 @@ function deviceShaken() {
 // (note that a die won't actually roll if it's frozen)
 // also, output the list of values to the console
 function shakeDice() {
-    for (let i = 0; i < dice.length; i++) {
+  for (let i = 0; i < dice.length; i++) {
     const die = dice[i];
     die.roll();
-      list = list + die.value + " ";
-      //for (x =0; x < count.length; x++);
-      sum += die.value; 
-      //for (let x =0; x < count.length; x++)
-      if(sum > 100) return;
-    
-  rolls++;
-    }
-  
-console.log(rolls);
-console.log(sum);
+    list = list + die.value + " ";
+    sum += die.value;
+    if (sum > 100) return;
 
+    rolls++;
+  }
+
+  console.log(rolls);
+  console.log(sum);
 }
